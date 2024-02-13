dff22d3bdf62d868182bf1af5c08edd2c894a5e6
Czaky <andrzej.w@lczak.net>
2024-02-13 13:32:23 +0000

    Better tests

--- rotated_array_test.py
+++ rotated_array_test.py
@@ -5,4 +5,4 @@ class TestRotatedArray(unittest.TestCase):
     def test_find_minimum(self):
         self.assertEqual(1, find_minimum([1, 2, 3, 4]))
         self.assertEqual(1, find_minimum([5, 1, 2, 3, 4]))
-        self.assertEqual(1, find_minimum([1, 2, 3, 4, 5]))
+        self.assertEqual(1, find_minimum([2, 3, 4, 5, 1]))
