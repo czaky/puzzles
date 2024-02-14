9434ef4561fc9a7a52cf07d1a0538d53a834ff0d
Czaky <andrzej.w@lczak.net>
2024-02-14 21:50:06 +0000

    meta cafe

--- arrays.py
+++ arrays.py
@@ -95,11 +95,11 @@ each other at a row of `n` seats. `s` is a list of taken seats.
 Seats are counted as [1 .. N] inclusive
 """
     s.sort()
-    # [1 - (d+1), S[0]] we can seat diners every (d + 1) seats
-    c = max(0, -1 + (s[0] + d) // (d + 1))
-    for i in range(1, len(s)):
+    return (
+        # [1 - (d+1), S[0]] we can seat diners every (d + 1) seats
+        max(0, -1 + (s[0] + d) // (d + 1)) +
         # We can seat a diner ever (d + 1) seats
-        c += max(0, -1 + (s[i] - s[i-1]) // (d + 1))
-    # [s[-1], n + (d+1)] we can seat diners ever (d + 1) seats.
-    c += (n - s[-1]) // (d + 1)
-    return c
+        sum(max(0, -1 + (s[i] - s[i-1]) // (d + 1))
+            for i in range(1, len(s))) +
+        # [s[-1], n + (d+1)] we can seat diners ever (d + 1) seats.
+        (n - s[-1]) // (d + 1))
