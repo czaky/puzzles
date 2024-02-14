81568228658e0f106d7d577ed04989b782ff0339
Czaky <andrzej.w@lczak.net>
2024-02-14 02:12:51 +0000

    typo

--- strings.py
+++ strings.py
@@ -12,7 +12,7 @@ def palindrome(s: str) -> bool:
     "True if `s` is a palindrome."
     # s == s[::-1]
     # Runs in O(N) time and O(1) space.
-    return next((False for i in range(len(s)//2) if s[i] !=s [-1-i]), True)
+    return next((False for i in range(len(s)//2) if s[i] != s[-1-i]), True)
 
 def anagram(a: str, b: str) -> bool:
     "True if `a` and `b` contain the same characters."
