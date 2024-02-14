e20ab4a2cc77daabc1af29d7ec29582cab66c2b7
Czaky <andrzej.w@lczak.net>
2024-02-14 16:08:52 +0000

    functional schedule

--- schedule.py
+++ schedule.py
@@ -1,16 +1,13 @@
 """Test module for the schedule or interval based puzzles."""
 
 from typing import List
+from functools import reduce
 
 def meetings(start: List[int], end: List[int]) -> int:
     """
 Returns maximum number of meetings that can be scheduled
 in one room based on the meetings' `start` and `end` times.
 """
-    t = -1
-    c = 0
-    for e, s in sorted(zip(end, start)):
-        if t < s:
-            t = e
-            c += 1
-    return c
+    return reduce(
+        (lambda v, m: (v[0]+1, m[0]) if v[1] < m[1] else v),
+        sorted(zip(end, start)), (0, -1))[0]
