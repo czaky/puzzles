0013e47063a4dee9c3ec24101f4f755a2afbcd7e
Czaky <andrzej.w@lczak.net>
2024-02-15 12:18:19 +0000

    heaps

---
+++ heaps.py
@@ -0,0 +1,17 @@
+"Module contains puzzles related to heaps."
+
+from heapq import heapify, heappush, heappop
+from typing import List
+
+def connect_ropes_cost(a: List[int]) -> int:
+    """
+Connect ropes of lengths in `a` into single one reducing the cost.
+Cost of connecting two ropes is equal to the sum of their lengths.
+"""
+    heapify(a)
+    c = 0
+    while len(a) > 1:
+        nr = heappop(a) + heappop(a)
+        heappush(a, nr)
+        c += nr
+    return c
---
+++ test_heaps.py
@@ -0,0 +1,11 @@
+"""Test module for the heap related puzzles."""
+import unittest
+import heaps as h
+
+class TestHeaps(unittest.TestCase):
+    """Test class for the heap related puzzles."""
+    def test_connect_ropes_cost(self):
+        "Test the `connect_ropes_cost` function."
+        self.assertEqual(0, h.connect_ropes_cost([1]))
+        self.assertEqual(5, h.connect_ropes_cost([2, 3]))
+        self.assertEqual(29, h.connect_ropes_cost([4, 3, 2, 6]))
