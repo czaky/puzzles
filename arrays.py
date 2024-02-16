8a4454520d8de61f82214cf33f4facce6a587600
Czaky <andrzej.w@lczak.net>
2024-02-16 12:18:26 +0000

    comment

--- arrays.py
+++ arrays.py
@@ -26,7 +26,6 @@ def rotated_minimum(a: list) -> int:
 
 def equilibrium_point(a: list) -> int:
     "Index in `a` where sums of elements before and after are equal."
-    # Uses binary search in O(log N).
     l = 0
     h = len(a) - 1
     l_sum = 0
