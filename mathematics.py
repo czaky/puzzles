6ea11e5582f77dad0e29aeef1296032c2a92e6d7
Czaky <andrzej.w@lczak.net>
2024-02-16 17:24:36 +0000

    binary_by_three

--- mathematics.py
+++ mathematics.py
@@ -24,3 +24,9 @@ def frog_hops(n: int) -> int:
     for i in range(4, n + 1):
         ways[i%4] = ways[(i-1)%4] + ways[(i-2)%4] + ways[(i-3)%4]
     return ways[n%4]
+
+def binary_string_by_three(s: str) -> bool:
+    "True if binary number in `s` is divisible by 3."
+    # difference of even and odd "1"s is divisible by 3
+    return (sum(c == "1" for i, c in enumerate(s) if i%2) -
+            sum(c == "1" for i, c in enumerate(s) if i%2 == 0))%3 == 0
