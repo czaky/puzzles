b4f6154a668396556106a4e77338b9d16a2ca3d5
Czaky <andrzej.w@lczak.net>
2024-03-09 23:44:45 +0000

    updated queens

--- board.py
+++ board.py
@@ -2,15 +2,14 @@
 
 def queens(n: int) -> list:
     "Return a list of queen combinations for an `NxN` board."
-    solutions = []
+    sols = []
     s = [0] * n
     def rec(r: int, invalid: int):
-        if r >= n:
-            solutions.append(s[:])
-            return
         place = 1 | 1 << (n+r) | 1 << (n*4-r)
         for s[r] in range(1, n+1):
-            _ = invalid & place or rec(r+1, invalid | place)
+            invalid & place or (
+                rec(r-1, invalid | place) if r > 0
+                else sols.append(s[:]))
             place <<= 1
-    rec(0, 0)
-    return solutions
+    n > 0 and rec(n-1, 0)
+    return sols
--- test_board.py
+++ test_board.py
@@ -7,8 +7,8 @@ class TestBoardGames(unittest.TestCase):
     """Test class for the board games puzzles."""
     def test_queens(self):
         "Test the `queens` puzzle."
-        self.assertEqual([[]], b.queens(0))
+        self.assertEqual([], b.queens(0))
         self.assertEqual([[1]], b.queens(1))
         self.assertEqual([], b.queens(2))
         self.assertEqual([], b.queens(3))
-        self.assertEqual([[2, 4, 1, 3], [3, 1, 4, 2]], b.queens(4))
+        self.assertEqual([[3, 1, 4, 2], [2, 4, 1, 3]], b.queens(4))
