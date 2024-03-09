97a6e3f1e0565d6f9c1bb37b4b3cdcef4cdcdd80
Czaky <andrzej.w@lczak.net>
2024-03-09 22:52:41 +0000

    update queens

--- board.py
+++ board.py
@@ -2,18 +2,20 @@
 
 def queens(n: int) -> list:
     "Return a list of queen combinations for an `NxN` board."
+    if n <= 0:
+        return []
     n4 = n*4
     solutions = []
     s = [0] * n
     def rec(r: int, invalid: int):
+        place = 1 | 1 << (n+r) | 1 << (n4-r)
         for c in range(n):
-            place = (1 << c) + + (1 << (n + c + r)) + (1 << (n4 + c - r))
-            if invalid & place:
-                continue
-            s[r] = c + 1
-            if r == 0:
-                solutions.append(s[:])
-                return
-            rec(r-1, invalid | place)
+            if not invalid & place:
+                s[r] = c + 1
+                if r == 0:
+                    solutions.append(s[:])
+                    return
+                rec(r-1, invalid | place)
+            place <<= 1
     rec(n-1, 0)
     return sorted(solutions)
