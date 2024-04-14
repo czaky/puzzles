741a9afda44bfa8674abc3f7aeca9c77ad70ca83
Czaky <andrzej.w@lczak.net>
2024-04-14 18:15:19 +0000

    smallest_intersecting_range

--- heaps.py
+++ heaps.py
@@ -1,7 +1,7 @@
 "Module contains puzzles related to heaps."
 
 from heapq import heapify, heappush, heappop
-from typing import List
+from typing import List, Tuple
 
 
 def connect_ropes_cost(a: List[int]) -> int:
@@ -16,3 +16,36 @@ def connect_ropes_cost(a: List[int]) -> int:
         heappush(a, nr)
         c += nr
     return c
+
+
+def smallest_intersecting_range(arrays: List[List[int]]) -> Tuple[int, int]:
+    "Return the smallest range that intersects the sorted `arrays`."
+    # The idea is to keep track of numbers from the `arrays` on the heap,
+    # always increasing the lowest number with the next on the same array.
+    # This approach tries to minimize the span of numbers on the heap.
+    # The heap then always has the `min and max values so far, which
+    # allow us to determine the running smallest range.
+
+    # Start with the lowest number in each array.
+    h = [(a[0], a, int(1)) for a in arrays]
+    heapify(h)  # min-heap
+
+    # The minimum is always h[0], but we need to track the heap max.
+    mxh = max(h)[0]  # max value on the heap, so far
+
+    # The range is stored as `(<length>, <start>)`,
+    # which allows to easily run `min` on it.
+    mnr = (mxh - h[0][0], h[0][0])  # minimum range, so far
+
+    while h:
+        v, a, c = heappop(h)
+        mnr = min(mnr, (mxh - v, v))
+        # Once we cannot add any more numbers from a list,
+        # we abort, otherwise the range would not include
+        # numbers from that particular list and would be invalid.
+        if c == len(a):
+            break
+        heappush(h, (a[c], a, c + 1))
+        mxh = max(mxh, a[c])
+
+    return mnr[1], mnr[0] + mnr[1]
--- test_heaps.py
+++ test_heaps.py
@@ -12,3 +12,30 @@ class TestHeaps(unittest.TestCase):
         self.assertEqual(0, h.connect_ropes_cost([1]))
         self.assertEqual(5, h.connect_ropes_cost([2, 3]))
         self.assertEqual(29, h.connect_ropes_cost([4, 3, 2, 6]))
+
+    def test_smallest_intersecting_rage(self):
+        "Test `smallest_intersecting_range`."
+        self.assertEqual(
+            (5, 8),
+            h.smallest_intersecting_range(
+                [[1, 3, 5], [7, 8, 9], [2, 4, 6], [2, 3, 8], [5, 7, 11]]
+            ),
+        )
+        self.assertEqual(
+            (501, 511),
+            h.smallest_intersecting_range(
+                [
+                    [10, 30, 500, 501],
+                    [70, 80, 509, 510],
+                    [20, 40, 506, 507],
+                    [20, 30, 508, 509],
+                    [50, 70, 511, 512],
+                ]
+            ),
+        )
+        self.assertEqual(
+            (1, 2),
+            h.smallest_intersecting_range(
+                [[1, 3, 5, 7, 9], [0, 2, 4, 6, 8], [2, 3, 5, 7, 11]]
+            ),
+        )
