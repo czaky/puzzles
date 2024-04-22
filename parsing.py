d7191e5b32e9a98978308af6f643621fffcdc82b
czaky <jendrzej.w@lczak.net>
2024-04-22 03:03:36 +0000

    parsing (IV) - extracted parser and formatter

--- parsing.py
+++ parsing.py
@@ -1,19 +1,26 @@
 "Algorithms used to parse and process formal languages."
 
-from typing import List, Sequence
+from typing import Callable, List, Sequence, Tuple
 
 
-def parse_binop_expression(tokens: Sequence) -> tuple:
+def expression_tuple(*args):
+    "Return args as a tuple."
+    return args
+
+
+def parse_binop_expression(tokens: Sequence, make: Callable = expression_tuple):
     """Parses an arithmetic expression consisting of literals and operators from '+-*/()'.
 
-    The parsed expression is represented as tripples of the form:
+    The parsed expression is represented as triples of the form:
         (<operator>, <arg1>, <arg2>)
+    Alternatively, a `make` function can be passed in for alternative representation.
 
     Parameters:
-        tokens (list): tokenized expression consisting of operators '+-*/()' and literals.
+        tokens (Sequence): tokenized expression consisting of operators '+-*/()' and literals.
+        make (Callable): generates binary expression objects out of arguments.
 
     Returns:
-        tuple: the root of the parsed expression as (<operator>, <arg1>, <arg2>)
+        any: the root of the parsed expression (as returned by the `make` function).
     """
     # The implementation here avoids recursion and uses two stacks
     # similar to the  Dijkstra's Shunting Yard algorithm.
@@ -21,8 +28,8 @@ def parse_binop_expression(tokens: Sequence) -> tuple:
     # Stores the arguments and successive parsed results of the expression.
     args = []
     # Stores the operators seen so far.
-    # The ops arrays should contains "+-(" and at most one of "*/".
-    # Note that we start with an implicit bracket.
+    # The ops array should only contain "+-(" and at most one of "*/".
+    # Start with an implicit, opening bracket.
     ops = ["("]
 
     def close_bracket():
@@ -39,9 +46,9 @@ def parse_binop_expression(tokens: Sequence) -> tuple:
         assert ops.pop() == "("
         # Apply the operators (in reversed order) to form the expressions.
         while rops:
-            rargs.append((rops.pop(), rargs.pop(), rargs.pop()))
+            rargs.append(make(rops.pop(), rargs.pop(), rargs.pop()))
         # The final expression is moved back onto the stack.
-        args.append(rargs.pop())
+        return rargs.pop()
 
     for t in tokens:
         # Push all operators onto the `ops` stack
@@ -50,28 +57,60 @@ def parse_binop_expression(tokens: Sequence) -> tuple:
             ops.append(t)
             continue
 
-        if t == ")":
-            # Closing bracket forms on new expression on the args stack.
-            close_bracket()
-        else:
-            # Push remaining args onto the stack.
-            args.append(t)
+        # Push remaining args onto the stack.
+        # Closing bracket also forms a new expression on the args stack.
+        args.append(close_bracket() if t == ")" else t)
 
         # Given a new expression on the stack from above,
         # we can now form any "*/" expressions if possible.
         # This removes all the "*/" expressions from the ops stack.
         while ops[-1] in "*/":
             b, a = args.pop(), args.pop()
-            args.append((ops.pop(), a, b))
+            args.append(make(ops.pop(), a, b))
 
-    # Finally, we close the first implicit bracket.
-    close_bracket()
-    # And return the remaining only expression on the args stack.
-    assert len(args) == 1
-    return args[0]
+    # Finally, we close the initial, implicit bracket.
+    return close_bracket()
 
 
-def format_binop_expression(e: str | tuple) -> str:
+def simplified_binop_expression_factory() -> Tuple[Callable, Callable]:
+    "Returns an expression formatter and a string extractor."
+    # Define the grammar:
+    # Precedence rank of the operators.
+    rank = {"*": 1, "/": 1, "+": 2, "-": 2}
+    # Do the operators need brackets for the right term?
+    commutative_operators = "+*"
+
+    # Is `v` a leaf (string) node?
+    leaf = lambda v: isinstance(v, str)
+    # Flatten a node into a string?
+    flat = lambda v: v if leaf(v) else v[1]
+    # Make a string out of argument `v` and surround it with brackets if necessary.
+    form = lambda v, flt: flat(v) if flt else "(" + flat(v) + ")"
+
+    def fop(op: str, a: str | tuple, b: str | tuple) -> tuple:
+        "Format an operation adding parentheses to the arguments if needed."
+        l = form(a, leaf(a) or rank[a[0]] <= rank[op])
+        r = form(b, leaf(b) or rank[b[0]] < rank[op] + (op in commutative_operators))
+        # Returns the operator and the string representation.
+        # We keep the operator so it is easier to know the type of the expression later.
+        return op, l + op + r
+
+    return fop, flat
+
+
+def simplify_binops_brackets_one_pass(tokens: Sequence) -> str:
+    "Remove redundant brackets from the expression in one pass."
+    # Provides an expression constructor, and
+    # an `extract` function to extract the string representation
+    # from the final expression.
+    expression, extract = simplified_binop_expression_factory()
+
+    return extract(parse_binop_expression(tokens, make=expression))
+
+
+def format_binop_expression(
+    e: str | tuple, factory=simplified_binop_expression_factory
+) -> str:
     "Format expressions consisting of (<operator>, <arg1>, <arg2>)."
     if isinstance(e, str):
         return e
@@ -79,25 +118,11 @@ def format_binop_expression(e: str | tuple) -> str:
     s: List[tuple] = []
     v: List[str | tuple] = []
     n: tuple | str | None = e
-    # Flatten an expression.
-    f = lambda v: v if isinstance(v, str) else v[1]
 
-    def fop(op: str, a: str | tuple, b: str | tuple) -> tuple:
-        "Format an operation adding parantheses to the arguments if needed."
-
-        def fr(v):
-            if isinstance(v, str) or op == "+" or op in "*-" and v[0] in "*/":
-                return f(v)
-            return "(" + f(v) + ")"
-
-        def fl(v):
-            if isinstance(v, str) or op in "+-" or v[0] in "*/":
-                return f(v)
-            return "(" + f(v) + ")"
-
-        # Returns the operator and the string representation.
-        # We keep the operator so it is easier to know the type of the expression later.
-        return op, fl(a) + op + fr(b)
+    # Provides an expression `factory`, and
+    # the `extract` function to extract the string representation
+    # from the final expression.
+    expression, extract = factory()
 
     while s or n:
         # Repeatedly, descend left from node.
@@ -117,100 +142,12 @@ def format_binop_expression(e: str | tuple) -> str:
             # Consume this node and
             # generate intermediate values.
             rv, lv = v.pop(), v.pop()
-            v.append(fop(n[0], lv, rv))
+            v.append(expression(n[0], lv, rv))
             n = None
-    return f(v[0])
+
+    return extract(v[0])
 
 
 def simplify_binops_brackets(tokens: Sequence) -> str:
     "Remove redundant brackets from the expression."
     return format_binop_expression(parse_binop_expression(tokens))
-
-
-def simplify_binops_brackets_one_pass(tokens: Sequence) -> str:
-    """Simplifies an arithmetic expression consisting of literals and operators from '+-*/()'.
-
-    This parses a binary opertation expression and formats it in one-pass.
-
-    Parameters:
-        tokens (list): tokenized expression consisting of operators '+-*/()' and literals.
-
-    Returns:
-        str: a string with redundant brackets removed
-    """
-    # The implementation here avoids recursion and uses two stacks
-    # similar to the  Dijkstra's Shunting Yard algorithm like above.
-    # The difference to `parse_binop_expression` is that the format operation
-    # is immediately applied without generating the intermediate syntax expression tuples.
-
-    # Stores the arguments and successive parsed results of the expression.
-    args = []
-    # Stores the operators seen so far.
-    # The ops arrays should contains "+-(" and at most one of "*/".
-    # Note that we start with an implicit bracket.
-    ops = ["("]
-
-    # Flatten an expression.
-    f = lambda v: v if isinstance(v, str) else v[1]
-
-    def fop(op: str, a, b) -> tuple:
-        "Format an operation adding parantheses to the arguments if needed."
-
-        def fr(v):
-            if isinstance(v, str) or op == "+" or op in "*-" and v[0] in "*/":
-                return f(v)
-            return "(" + f(v) + ")"
-
-        def fl(v):
-            if isinstance(v, str) or op in "+-" or v[0] in "*/":
-                return f(v)
-            return "(" + f(v) + ")"
-
-        # Returns the operator and the string representation.
-        # We keep the operator so it is easier to know the type of the expression later.
-        return op, fl(a) + op + fr(b)
-
-    def close_bracket():
-        # This is called when we close and process a (bracketed) sub-expression
-        #
-        # The remaining operators here are "+-".
-        # The idea is to first reverse the operators and their arguments.
-        rargs = [args.pop()]
-        rops = []
-        while ops[-1] in "+-":
-            rargs.append(args.pop())
-            rops.append(ops.pop())
-        # We should have arrived at the opening bracket.
-        assert ops.pop() == "("
-        # Apply the operators (in reversed order) to form the expressions.
-        while rops:
-            rargs.append(fop(rops.pop(), rargs.pop(), rargs.pop()))
-        # The final expression is moved back onto the stack.
-        args.append(rargs.pop())
-
-    for t in tokens:
-        # Push all operators onto the `ops` stack
-        # except for the closing bracket.
-        if t in "*/+-(":
-            ops.append(t)
-            continue
-
-        if t == ")":
-            # Closing bracket forms on new expression on the args stack.
-            close_bracket()
-        else:
-            # Push remaining args onto the stack.
-            args.append(t)
-
-        # Given a new expression on the stack from above,
-        # we can now form any "*/" expressions if possible.
-        # This removes all the "*/" expressions from the ops stack.
-        while ops[-1] in "*/":
-            b, a = args.pop(), args.pop()
-            args.append(fop(ops.pop(), a, b))
-
-    # Finally, we close the first implicit bracket.
-    close_bracket()
-    # And return the remaining only expression on the args stack.
-    assert len(args) == 1
-    return f(args[0])
