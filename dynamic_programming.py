2bbda50bb170c40ca0e060815fe18ba61633dece
czaky <jendrzej.w@lczak.net>
2024-05-04 03:19:26 +0000

    revisit prime_product_subset_count

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -46,7 +46,7 @@ PRIMES_10_MASKS = [
 ]
 
 
-def prime_product_subset_count(a: List[int], m: int = 10**9 + 7) -> int:
+def prime_product_subset_count(a: List[int], mod: int = 10**9 + 7) -> int:
     """
     Given array `a` count the number of subsets of `a`,
     for which the product consist of distinct primes from 2 to 29.
@@ -55,7 +55,7 @@ def prime_product_subset_count(a: List[int], m: int = 10**9 + 7) -> int:
     ----------
     a : List[int]
         A list of numbers from 1 to 30 (inclusive)
-    m : int
+    mod : int
         A modulo value. Defaults to: 10**9 + 7
 
     Returns
@@ -71,21 +71,17 @@ def prime_product_subset_count(a: List[int], m: int = 10**9 + 7) -> int:
     # by the count of subsets without the new
     # primes in the number (j & mask != 0) multiplied by the count
     # of the number in the `a` array.
-    counts = Counter(a)
-    dp = [0] * PRIMES_10_BITS
-    dp[0] = 1
-
-    for i, cnt in counts.items():
-        # bits representing the primes that n is a product of
-        mask = PRIMES_10_MASKS[i]
-        if cnt and mask:
-            # Iterate over the existing 1024 prime_masks.
-            for j in range(PRIMES_10_BITS):
-                # In case new primes can be added to the mask.
-                if j & mask == 0:
-                    # The new count of subsets will be
-                    # `dp[j]` * the count of the number in `a`
-                    dp[j | mask] += dp[j] * cnt % m
-    # Return the sum of counts for all possible sets of primes
-    # minus empty set. Multiply by 2^(count of 1s).
-    return ((sum(dp) - 1) * pow(2, counts[1], m)) % m
+    mcs = tuple(Counter(PRIMES_10_MASKS[e] for e in a).items())
+    # Initialize with 1, which is only important for `dp[0]`.
+    dp = [1] * (1 << len(PRIMES_10))
+    # Build the `dp` array iteratively.
+    # `dp[j]` is the sum of all previously encountered sets
+    # without the specific mask (of prime factor)
+    # multiplied by the counter of numbers with the specific mask.
+    # The condition after `if` make sure that
+    # `j` is a proper superset of `j ^ mask`.
+    # And it makes sure, that we don't count symmetric subsets
+    # twice: `j ^ m < m`.
+    for j in range(1, len(dp)):
+        dp[j] = sum(dp[j ^ m] * c % mod for m, c in mcs if j ^ m < m == j & m)
+    return (sum(dp) - 1) * 2 ** a.count(1)
