77908e1f3ff9a627748f7b30d1cbd4b2348a0622
czaky <jendrzej.w@lczak.net>
2024-05-05 23:51:42 +0000

    lint (all: 8)

--- stack.py
+++ stack.py
@@ -10,7 +10,7 @@ from typing import Any, Iterable, Iterator, Sequence, Set
 class IndexStack(Sequence, Set):
     """A set that is also a stack. Keeps track of elements' positions."""
 
-    def __init__(self, iterable=()) -> None:
+    def __init__(self, iterable: Iterable = ()) -> None:
         """Create an IndexStack from `iterable`.
 
         Parameters
@@ -22,13 +22,13 @@ class IndexStack(Sequence, Set):
         self.stack = list(iterable)
         self.pos = {e: i + 1 for i, e in enumerate(self.stack)}
 
-    def push(self, n) -> None:
+    def push(self, n: Any) -> None:
         """Push `n` onto the stack adding it to the set if not present yet."""
         if n not in self.pos:
             self.pos[n] = len(self.stack)
             self.stack.append(n)
 
-    def pop(self, default=None) -> Any:
+    def pop(self, default: Any = None) -> Any:
         """Pop the last element from the stack and remove it from the set."""
         if self.stack:
             e = self.stack.pop()
@@ -36,7 +36,7 @@ class IndexStack(Sequence, Set):
             return e
         return default
 
-    def top(self, default=None) -> Any:
+    def top(self, default: Any = None) -> Any:
         """Return the top element of the stack."""
         return self.stack[-1] if self.stack else default
 
@@ -50,7 +50,7 @@ class IndexStack(Sequence, Set):
             return removed
         return [][:]
 
-    def cut_from(self, n) -> list:
+    def cut_from(self, n: Any) -> list:
         """Cut off the stack down from element `n`. Return removed ones."""
         i = self.pos.get(n)
         return [] if i is None else self.cut(i)
@@ -68,7 +68,7 @@ class IndexStack(Sequence, Set):
         """Return the count of `value` on the stack."""
         return 1 if value in self.pos else 0
 
-    def index(self, value: object, start: int = 0, stop=None) -> int:  # noqa: ARG002
+    def index(self, value: object, start: int = 0, stop: int | None = None) -> int:  # noqa: ARG002
         """Index of the first occurence of `value` in the stack."""
         return self.pos[value]
 
@@ -82,27 +82,35 @@ class IndexStack(Sequence, Set):
             self.push(e)
 
     def __len__(self) -> int:
+        """Return the depth of this stack."""
         return len(self.stack)
 
     def __bool__(self) -> bool:
+        """Return True if stack is not empty."""
         return len(self.stack) > 0
 
     def __contains__(self, value: object) -> bool:
+        """Return True if `value` is in the stack."""
         return value in self.pos
 
     def __str__(self) -> str:
+        """Return string representation of the stack."""
         return str(self.stack)
 
     def __iter__(self) -> Iterator:
+        """Iterate over the stack."""
         yield from self.stack
 
     def __reversed__(self) -> Iterator:
+        """Return reversed iterator over the stack."""
         return reversed(self.stack)
 
-    def __getitem__(self, i: int | slice):
+    def __getitem__(self, i: int | slice) -> Any:
+        """Get an item or a slice of the stack, counting from bottom."""
         return self.stack[i]
 
     def __delitem__(self, i: int | slice) -> None:
+        """Remove an item or a slice from the stack."""
         if isinstance(i, slice):
             for e in self.stack[i]:
                 del self.pos[e]
@@ -111,6 +119,7 @@ class IndexStack(Sequence, Set):
         del self.stack[i]
 
     def __eq__(self, other: object) -> bool:
+        """Test if two stacks are equal."""
         return isinstance(other, IndexStack) and self.stack == other.stack
 
 
@@ -118,6 +127,14 @@ class CountedStack(Sequence):
     """Implementation of a stack, that counts its elements."""
 
     def __init__(self, it: Iterable = ()) -> None:
+        """Initialize the CountedStack from the Iterable.
+
+        Parameters
+        ----------
+        it : Iterable, optional
+            initial contents of the stack, by default ()
+
+        """
         self.stack = list(it)
         self.counter = Counter(self.stack)
 
@@ -126,7 +143,7 @@ class CountedStack(Sequence):
         self.stack.append(e)
         self.counter[e] += 1
 
-    def pop(self, default=None) -> Any:
+    def pop(self, default: Any = None) -> Any:
         """Pop element `e` from the stack."""
         if self.stack:
             e = self.stack.pop()
@@ -136,11 +153,11 @@ class CountedStack(Sequence):
             return e
         return default
 
-    def top(self, default=None) -> Any:
+    def top(self, default: Any = None) -> Any:
         """Return the top element of the stack."""
         return self.stack[-1] if self.stack else default
 
-    def cut(self, size) -> list[Any]:
+    def cut(self, size: int) -> list[Any]:
         """Shrink the stack to `size`. Returns cut elements in FIFO order."""
         if len(self.stack) > size:
             removed = self.stack[size:]
@@ -165,7 +182,7 @@ class CountedStack(Sequence):
         """Return the count of `value` on the stack."""
         return self.counter[value]
 
-    def index(self, value: object, start: int = 0, stop=None) -> int:
+    def index(self, value: object, start: int = 0, stop: Any = None) -> int:
         """Index of the first occurrence of `value` in the stack."""
         if self.counter[value] > 0:
             return self.stack.index(value, start, stop)
@@ -183,24 +200,31 @@ class CountedStack(Sequence):
         self.counter.update(islice(self.stack, sl, None))
 
     def __len__(self) -> int:
+        """Return the depth of this stack."""
         return len(self.stack)
 
     def __bool__(self) -> bool:
+        """Return True if stack is not empty."""
         return len(self.stack) > 0
 
     def __iter__(self) -> Iterator:
+        """Iterate over the stack."""
         yield from self.stack
 
     def __reversed__(self) -> Iterator:
+        """Return reversed iterator over the stack."""
         return reversed(self.stack)
 
     def __contains__(self, value: object) -> bool:
+        """Return True if `value` is in the stack."""
         return self.counter[value] > 0
 
-    def __getitem__(self, i: int | slice):
+    def __getitem__(self, i: int | slice) -> Any:
+        """Get an item or a slice of the stack, counting from bottom."""
         return self.stack[i]
 
     def __delitem__(self, i: int | slice) -> None:
+        """Remove an item or a slice from the stack."""
         if isinstance(i, slice):
             self.counter.subtract(self.stack[i])
             for e in self.stack[i]:
@@ -215,7 +239,9 @@ class CountedStack(Sequence):
         del self.stack[i]
 
     def __eq__(self, other: object) -> bool:
+        """Test if two stacks are equal."""
         return isinstance(other, CountedStack) and self.stack == other.stack
 
     def __str__(self) -> str:
+        """Return string representation of the stack."""
         return str(self.stack)
